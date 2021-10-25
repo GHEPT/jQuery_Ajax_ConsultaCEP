957e4645d96992b09159a70e29d6ee9e0ac84a3f
GHEPT <teodoro.edu@hotmail.com>
2021-10-25 20:14:48 -0300

    [FIX] Background

--- js/main.js
+++ js/main.js
@@ -2,11 +2,32 @@ function consultaCEP(){
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/"+ cep +"/json/";
     console.log(url);
-    $.ajax({
-        url: url,
-        type: "GET",
-        success: function(response){
-            console.log(response);
-        }
-    })
+
+    if ($.trim($("#cep").val()) != ""){
+        $(".barra-progresso").show();
+        $.ajax({
+            url: url,
+            type: "GET",
+            success: function(response){                
+                    console.log(response);
+                    $("#logradouro").html(response.logradouro);
+                    $("#bairro").html(response.bairro);
+                    $("#localidade").html(response.localidade);
+                    $("#uf").html(response.uf);
+                    $("#tituloCEP").html("CEP " + response.cep);
+                    $(".cep").show();
+                    $("#cep").val(""); // limpa o CEP digitado após o envio
+                    $(".barra-progresso").hide();
+            }
+        })
+    } else {
+        alert("Por favor, insira um CEP");
+    }
+
+    
 }
+
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+});
